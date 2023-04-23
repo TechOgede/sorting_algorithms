f9983720c87a8ee49b5da5099a8d7ad1fb2ea4b2
TechOgede <sammonius@live.com>
2023-04-23 08:33:21 -0700

    put swap func back in files

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -2,6 +2,25 @@
 
 
 /**
+* swap - swaps two values
+* @n1: first value
+* @n2: second value
+*
+* Description: Basic swap using a temp variable
+*/
+
+void swap(int *n1, int *n2)
+{
+	int temp;
+
+	temp = *n1;
+	*n1 = *n2;
+	*n2 = temp;
+}
+
+
+
+/**
 * selection_sort - Implementation of the selection sort algorithm
 * @array: array of ints
 * @size: size of the array
--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -2,6 +2,24 @@
 
 
 /**
+* swap - swaps two values
+* @n1: first value
+* @n2: second value
+*
+* Description: Basic swap using a temp variable
+*/
+
+void swap(int *n1, int *n2)
+{
+	int temp;
+
+	temp = *n1;
+	*n1 = *n2;
+	*n2 = temp;
+}
+
+
+/**
 * partition - meat of the quick sort algorithm
 * @array: ponter to an array
 * @lb: lower bound of the array
